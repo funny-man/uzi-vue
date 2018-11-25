fcae304e9fb32acd76efbc0811ef97eba57fc7a4
zeeliu <zeeliu666@163.com>
2018-11-25 17:56:53 +0800

    添加var.scss定义字体颜色

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-    <ul>
-      <li>ssss</li>
-      <li>ddd</li>
-      <li>fff</li>
-      <li>ggg</li>
-    </ul>
+    <z-button shadow block>Large Button</z-button>
+    <div class="gap"/>
+    <z-button edge>Edge Button</z-button>
+    <div class="gap"/>
+    <z-button round>Round Button</z-button>
+    <div class="gap"/>
+    <z-button size="medium">Medium Button</z-button>
+    <div class="gap"/>
+    <z-button size="small">Small Button</z-button>
   </div>
 </template>
 
 <script>
+import Button from './components/button/button.vue';
+
 export default {
   name: 'app',
   components: {
+    'z-button': Button,
   },
 };
 </script>
@@ -20,11 +26,14 @@ export default {
 <style lang="scss">
 @import "./sass/reset.scss";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 30px 30px;
+}
+.gap {
+  height: 30px;
 }
 </style>
---
+++ src/components/button/button.vue
@@ -0,0 +1,102 @@
+<template>
+  <button class="z-button"
+          type="button"
+          :class="[size,{'is-round':round,'is-edge':edge,'is-shadow':shadow,'is-block':block}]"
+  >
+  <svg class="icon">
+    <use class="icon-font" xlink:href="#i-loading"></use>
+  </svg>
+  <slot></slot>
+  </button>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'z-button',
+  props: {
+    size: {
+      type: String,
+      default: 'large',
+    },
+    round: {
+      type: Boolean,
+      default: false,
+    },
+    circle: {
+      type: Boolean,
+      default: false,
+    },
+    edge: {
+      type: Boolean,
+      default: false,
+    },
+    shadow: {
+      type: Boolean,
+      default: false,
+    },
+    block: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import '../../sass/var.scss';
+@import '../../sass/animation.scss';
+  .icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+    margin-right: 8px;
+    @include rotate-line;
+    opacity: .6;
+  }
+  .z-button {
+    margin: 0;
+    padding: 0;
+    border: none;
+    background-color: $color-primary;
+    border-radius: $radius-size-base;
+  }
+  .is-block {
+    width: 100%;
+  }
+  .is-shadow {
+    box-shadow: $btn-shadow-base;
+  }
+  .is-round {
+    border-radius: $radius-size-round;
+  }
+  .is-circle {
+    border-radius: $radius-size-circle;
+  }
+  .is-edge {
+    border-radius: 0;
+  }
+  .large {
+    color: $fontColor-white;
+    font-size: $fontSize-s;
+    font-weight: $fontWeigth-bold;
+    padding: 14px 22px;
+  }
+  .medium {
+    color: $fontColor-white;
+    font-size: $fontSize-s;
+    font-weight: $fontWeigth-bold;
+    padding: 10px 22px;
+  }
+  .small {
+    color: $fontColor-white;
+    font-size: $fontSize-ss;
+    font-weight: $fontWeigth-bold;
+    padding: 7px 16px;
+  }
+</style>
