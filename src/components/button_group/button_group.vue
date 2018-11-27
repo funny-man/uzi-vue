a458da520f9f4f60d0b01959dd22eb17ea07ac27
zeeliu <zeeliu666@163.com>
2018-11-27 18:48:42 +0800

    button-group未完成

---
+++ src/components/button_group/button_group.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="z-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'z-button-group',
+  props: {
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../sass/var.scss";
+@import "../../sass/animation.scss";
+.z-button-group {
+  .z-button {
+    border-radius: 0;
+    &:first-child {
+      border-top-left-radius: $radius-size-base;
+      border-bottom-left-radius: $radius-size-base;
+    }
+    &:last-child {
+      border-top-right-radius: $radius-size-base;
+      border-bottom-right-radius: $radius-size-base
+    }
+  }
+}
+</style>
