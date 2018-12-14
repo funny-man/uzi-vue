3f689266e1de36a6f88b9d41fe5714fb59a7ee19
zeeliu <zeeliu666@163.com>
2018-12-14 12:47:52 +0800

    测试button--->slot

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { mount } from '@vue/test-utils';
 import Button from '@/components/button/button.vue';
+import Icon from '@/components/icon/icon.vue';
 
 describe('Button.vue', () => {
   // 现在挂载组件，你便得到了这个包裹器
@@ -26,34 +27,102 @@ describe('Button.vue', () => {
     // expect(wrapper.attributes('class')).to.include('is-disabled');
   });
   it('改变button尺寸', () => {
-    expect(wrapper.attributes('class')).to.include('large');
+    expect(wrapper.classes().indexOf('large')).to.not.eq(-1);
     let size = 'medium';
     wrapper.setProps({ size });
-    expect(wrapper.attributes('class')).to.include('medium');
+    expect(wrapper.classes().indexOf('medium')).to.not.eq(-1);
     size = 'small';
     wrapper.setProps({ size });
-    expect(wrapper.attributes('class')).to.include('small');
+    expect(wrapper.classes().indexOf('small')).to.not.eq(-1);
+    // expect(wrapper.classes()).to.include('small');
   });
   it('改变button类型', () => {
-    expect(wrapper.attributes('class')).to.include('default');
+    expect(wrapper.classes().indexOf('type-default')).to.not.eq(-1);
     let type = 'primary';
     wrapper.setProps({ type });
-    expect(wrapper.attributes('class')).to.include('primary');
+    expect(wrapper.classes().indexOf('type-primary')).to.not.eq(-1);
     type = 'success';
     wrapper.setProps({ type });
-    expect(wrapper.attributes('class')).to.include('success');
+    expect(wrapper.classes().indexOf('type-success')).to.not.eq(-1);
     type = 'warning';
     wrapper.setProps({ type });
-    expect(wrapper.attributes('class')).to.include('warning');
+    expect(wrapper.classes().indexOf('type-warning')).to.not.eq(-1);
     type = 'error';
     wrapper.setProps({ type });
-    expect(wrapper.attributes('class')).to.include('error');
+    expect(wrapper.classes().indexOf('type-error')).to.not.eq(-1);
+  });
+  it('设置成幽灵按钮', () => {
+    expect(wrapper.classes().indexOf('is-ghost')).to.eq(-1);
+    const ghost = true;
+    wrapper.setProps({ ghost });
+    expect(wrapper.classes().indexOf('is-ghost')).to.not.eq(-1);
+  });
+  it('设置成虚线按钮', () => {
+    expect(wrapper.classes().indexOf('is-dashed')).to.eq(-1);
+    const dashed = true;
+    wrapper.setProps({ dashed });
+    expect(wrapper.classes().indexOf('is-dashed')).to.not.eq(-1);
+  });
+  it('设置成大圆角按钮', () => {
+    expect(wrapper.classes().indexOf('is-round')).to.eq(-1);
+    const round = true;
+    wrapper.setProps({ round });
+    expect(wrapper.classes().indexOf('is-round')).to.not.eq(-1);
+  });
+  it('设置成圆形按钮', () => {
+    expect(wrapper.classes().indexOf('is-circle')).to.eq(-1);
+    const circle = true;
+    wrapper.setProps({ circle });
+    expect(wrapper.classes().indexOf('is-circle')).to.not.eq(-1);
+  });
+  it('设置成锋利的直角按钮', () => {
+    expect(wrapper.classes().indexOf('is-edge')).to.eq(-1);
+    const edge = true;
+    wrapper.setProps({ edge });
+    expect(wrapper.classes().indexOf('is-edge')).to.not.eq(-1);
+  });
+  it('为按钮设置阴影', () => {
+    expect(wrapper.classes().indexOf('is-shadow')).to.eq(-1);
+    const shadow = true;
+    wrapper.setProps({ shadow });
+    expect(wrapper.classes().indexOf('is-shadow')).to.not.eq(-1);
+  });
+  it('设置为全宽的按钮', () => {
+    expect(wrapper.classes().indexOf('is-block')).to.eq(-1);
+    const block = true;
+    wrapper.setProps({ block });
+    expect(wrapper.classes().indexOf('is-block')).to.not.eq(-1);
+  });
+  it('设置为loading状态', () => {
+    const loading = true;
+    wrapper.setProps({ loading });
+    const useElement = wrapper.find('use');
+    expect(useElement.attributes('href')).to.eq('#i-loading');
+    // 还原loading参数方便测试下面的icon参数
+    wrapper.setProps({ loading: false });
   });
   it('正确添加icon', () => {
+    expect(wrapper.contains(Icon)).to.be.false;
     const icon = 'settings';
     wrapper.setProps({ icon });
     // 选中子组件
     const useElement = wrapper.find('use');
+    expect(wrapper.contains(Icon)).to.be.true;
     expect(useElement.attributes('href')).to.eq(`#i-${icon}`);
+  });
+  it('设置icon的位置', () => {
+    expect(wrapper.classes().indexOf('icon-left')).to.not.eq(-1);
+    expect(wrapper.classes().indexOf('icon-right')).to.eq(-1);
+    const iconPosition = 'right';
+    wrapper.setProps({ iconPosition });
+    expect(wrapper.classes().indexOf('icon-left')).to.eq(-1);
+    expect(wrapper.classes().indexOf('icon-right')).to.not.eq(-1);
+  });
+  xit('测试slot的内容', () => {
+    // expect(wrapper.attributes('class')).to.include('icon-left');
+    // const iconPosition = 'right';
+    // wrapper.setProps({ iconPosition });
+    // expect(wrapper.attributes('class')).to.include('icon-center');
+    // wrapper.setSlot ({ iconPosition });
   });
 });
