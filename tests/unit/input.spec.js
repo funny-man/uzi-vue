9ac27c2facabeb04cc6fbeac77e77433fa3f57f6
zeeliu <zeeliu666@163.com>
2018-12-18 19:35:36 +0800

    合并相似测试用例

--- tests/unit/input.spec.js
+++ tests/unit/input.spec.js
@@ -65,58 +65,71 @@ describe('Input.vue', () => {
     expect(wrapper.find(Icon).isVisible()).to.be.true;
     expect(wrapper.classes().indexOf('is-suffix')).to.not.eq(-1);
   });
-  it('测试change事件', () => {
+  it('测试change/input/focus/blur事件', () => {
     const inputWrapper = wrapper.find('input');
     const clickHandler = sinon.stub();
-    inputWrapper.element.addEventListener('change', clickHandler);
-    // 由于inputWrapper不是组件实例所以不能用wrapper.trigger触发事件
-    // 采用原生js方法做浏览器兼容判断（测试只用于chrome所以可以只写兼容chrome的方法）
-    const evt = document.createEvent('HTMLEvents');
-    evt.initEvent('change', false, true);
-    inputWrapper.element.dispatchEvent(evt);
-    expect(wrapper.emitted().change).to.exist;
-    expect(clickHandler.called).to.be.true;
-    expect(clickHandler.calledWith(evt)).to.be.true;
+    ['change', 'input', 'focus', 'blur'].forEach((eventName) => {
+      inputWrapper.element.addEventListener(eventName, clickHandler);
+      const evt = document.createEvent('HTMLEvents');
+      evt.initEvent(eventName, false, true);
+      inputWrapper.element.dispatchEvent(evt);
+      expect(wrapper.emitted()[eventName]).to.exist;
+      expect(clickHandler.called).to.be.true;
+      expect(clickHandler.calledWith(evt)).to.be.true;
+    });
   });
-  it('测试input事件', () => {
-    const inputWrapper = wrapper.find('input');
-    const clickHandler = sinon.stub();
-    inputWrapper.element.addEventListener('input', clickHandler);
-    // 由于inputWrapper不是组件实例所以不能用wrapper.trigger触发事件
-    // 采用原生js方法做浏览器兼容判断（测试只用于chrome所以可以只写兼容chrome的方法）
-    const evt = document.createEvent('HTMLEvents');
-    evt.initEvent('input', false, true);
-    inputWrapper.element.dispatchEvent(evt);
-    expect(wrapper.emitted().input).to.exist;
-    expect(clickHandler.called).to.be.true;
-    expect(clickHandler.calledWith(evt)).to.be.true;
-  });
-  it('测试吧focus事件', () => {
-    const inputWrapper = wrapper.find('input');
-    const clickHandler = sinon.stub();
-    inputWrapper.element.addEventListener('focus', clickHandler);
-    // 由于inputWrapper不是组件实例所以不能用wrapper.trigger触发事件
-    // 采用原生js方法做浏览器兼容判断（测试只用于chrome所以可以只写兼容chrome的方法）
-    const evt = document.createEvent('HTMLEvents');
-    evt.initEvent('focus', false, true);
-    inputWrapper.element.dispatchEvent(evt);
-    expect(wrapper.emitted().focus).to.exist;
-    expect(clickHandler.called).to.be.true;
-    expect(clickHandler.calledWith(evt)).to.be.true;
-  });
-  it('测试吧blur事件', () => {
-    const inputWrapper = wrapper.find('input');
-    const clickHandler = sinon.stub();
-    inputWrapper.element.addEventListener('blur', clickHandler);
-    // 由于inputWrapper不是组件实例所以不能用wrapper.trigger触发事件
-    // 采用原生js方法做浏览器兼容判断（测试只用于chrome所以可以只写兼容chrome的方法）
-    const evt = document.createEvent('HTMLEvents');
-    evt.initEvent('blur', false, true);
-    inputWrapper.element.dispatchEvent(evt);
-    expect(wrapper.emitted().blur).to.exist;
-    expect(clickHandler.called).to.be.true;
-    expect(clickHandler.calledWith(evt)).to.be.true;
-  });
+  // it('测试change事件', () => {
+  //   const inputWrapper = wrapper.find('input');
+  //   const clickHandler = sinon.stub();
+  //   inputWrapper.element.addEventListener('change', clickHandler);
+  //   // 由于inputWrapper不是组件实例所以不能用wrapper.trigger触发事件
+  //   // 采用原生js方法做浏览器兼容判断（测试只用于chrome所以可以只写兼容chrome的方法）
+  //   const evt = document.createEvent('HTMLEvents');
+  //   evt.initEvent('change', false, true);
+  //   inputWrapper.element.dispatchEvent(evt);
+  //   expect(wrapper.emitted().change).to.exist;
+  //   expect(clickHandler.called).to.be.true;
+  //   expect(clickHandler.calledWith(evt)).to.be.true;
+  // });
+  // it('测试input事件', () => {
+  //   const inputWrapper = wrapper.find('input');
+  //   const clickHandler = sinon.stub();
+  //   inputWrapper.element.addEventListener('input', clickHandler);
+  //   // 由于inputWrapper不是组件实例所以不能用wrapper.trigger触发事件
+  //   // 采用原生js方法做浏览器兼容判断（测试只用于chrome所以可以只写兼容chrome的方法）
+  //   const evt = document.createEvent('HTMLEvents');
+  //   evt.initEvent('input', false, true);
+  //   inputWrapper.element.dispatchEvent(evt);
+  //   expect(wrapper.emitted().input).to.exist;
+  //   expect(clickHandler.called).to.be.true;
+  //   expect(clickHandler.calledWith(evt)).to.be.true;
+  // });
+  // it('测试吧focus事件', () => {
+  //   const inputWrapper = wrapper.find('input');
+  //   const clickHandler = sinon.stub();
+  //   inputWrapper.element.addEventListener('focus', clickHandler);
+  //   // 由于inputWrapper不是组件实例所以不能用wrapper.trigger触发事件
+  //   // 采用原生js方法做浏览器兼容判断（测试只用于chrome所以可以只写兼容chrome的方法）
+  //   const evt = document.createEvent('HTMLEvents');
+  //   evt.initEvent('focus', false, true);
+  //   inputWrapper.element.dispatchEvent(evt);
+  //   expect(wrapper.emitted().focus).to.exist;
+  //   expect(clickHandler.called).to.be.true;
+  //   expect(clickHandler.calledWith(evt)).to.be.true;
+  // });
+  // it('测试吧blur事件', () => {
+  //   const inputWrapper = wrapper.find('input');
+  //   const clickHandler = sinon.stub();
+  //   inputWrapper.element.addEventListener('blur', clickHandler);
+  //   // 由于inputWrapper不是组件实例所以不能用wrapper.trigger触发事件
+  //   // 采用原生js方法做浏览器兼容判断（测试只用于chrome所以可以只写兼容chrome的方法）
+  //   const evt = document.createEvent('HTMLEvents');
+  //   evt.initEvent('blur', false, true);
+  //   inputWrapper.element.dispatchEvent(evt);
+  //   expect(wrapper.emitted().blur).to.exist;
+  //   expect(clickHandler.called).to.be.true;
+  //   expect(clickHandler.calledWith(evt)).to.be.true;
+  // });
   it('销毁实例', () => {
     wrapper.destroy();
   });
