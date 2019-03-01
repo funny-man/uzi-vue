2d832f6b2264b9a96f090c9b4781e517d7c1a170
zeeliu <zeeliu666@163.com>
2019-03-01 11:15:58 +0800

    添加toast组件

---
+++ src/components/toast/toast.vue
@@ -0,0 +1,25 @@
+/*
+ * @Author: zee
+ * @Date: 2018-12-28 15:11:18
+ * @Last Modified by: zee
+ * @Last Modified time: 2018-12-28 18:03:21
+ */
+<template>
+  <div class="z-toast">
+    {{txt}}
+    <slot></slot>  
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: 'z-toast',
+  props: {
+    txt: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+</style>
