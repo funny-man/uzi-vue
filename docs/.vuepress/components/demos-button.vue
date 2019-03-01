9acae440a4d0e952b8bf49e038c7156367c8847f
zeeliu <zeeliu666@163.com>
2019-03-01 12:53:45 +0800

    icon 测试

--- docs/.vuepress/components/demos-button.vue
+++ docs/.vuepress/components/demos-button.vue
@@ -9,9 +9,7 @@
         <h5>按钮类型</h5>
         <p>按钮有四种类型：主按钮、次按钮、幽灵按钮、虚线按钮。</p>
       </div>
-      <pre>
-        <code v-text="buttonType"></code>
-      </pre>
+      <pre><code v-text="buttonType"></code></pre>
     </div>
 
     <div class="demo-warpper">
@@ -22,27 +20,22 @@
         <h5>按钮尺寸</h5>
         <p>按钮有大、中、小三种尺寸。通过设置 size 为 Medium small 分别把按钮设为中、小尺寸。若不设置 size，则尺寸为大</p>
       </div>
-      <pre>
-        <code v-text="buttonSize"></code>
-      </pre>
+      <pre><code v-text="buttonSize"></code></pre>
     </div>
 
     <div class="demo-warpper">
       <z-button type="primary" ghost>默认</z-button>
       <z-button type="primary" ghost round>Round</z-button>
       <z-button type="primary" ghost edge>Edge</z-button>
-      <!-- <z-button type="primary" loading ghost circle></z-button> -->
+      <z-button type="primary" loading ghost circle></z-button>
       <div class="text">
         <h5>按钮倒角</h5>
         <p>按钮有正常、直角、圆角、圆形。通过设置 round edge circle 把按钮设置为圆角 直角 圆形 默认为小倒角</p>
       </div>
-      <pre>
-        <code v-text="buttonSize"></code>
-      </pre>
+      <pre><code v-text="buttonBorder"></code></pre>
     </div>
   </div>
 </template>
-<script src="//at.alicdn.com/t/font_930724_t8wqkiim6g.js"></script>
 <script>
   import Button from '../../../src/components/button/button.vue';
   import Icon from '../../../src/components/icon/icon.vue';
@@ -58,13 +51,18 @@
         isLoading: false,
         buttonType:`    
     <z-button type="primary" size="medium" shadow>Primary</z-button>
-    <z-button size="medium" shadow>Default</z-button>
-    <z-button type="primary" ghost size="medium" shadow>幽灵按钮</z-button>
-    <z-button type="primary" dashed size="medium" shadow>虚线按钮</z-button>`,
+  <z-button size="medium" shadow>Default</z-button>
+  <z-button type="primary" ghost size="medium" shadow>幽灵按钮</z-button>
+  <z-button type="primary" dashed size="medium" shadow>虚线按钮</z-button>`.trim(),
         buttonSize:`
     <z-button type="primary" ghost>Large Button</z-button>
-    <z-button type="primary" ghost size="medium">Medium Button</z-button>
-    <z-button type="primary" ghost size="small">Small Button</z-button>`,
+  <z-button type="primary" ghost size="medium">Medium Button</z-button>
+  <z-button type="primary" ghost size="small">Small Button</z-button>`.trim(),
+        buttonBorder:`
+    <z-button type="primary" ghost>默认</z-button>
+  <z-button type="primary" ghost round>Round</z-button>
+  <z-button type="primary" ghost edge>Edge</z-button>
+  <z-button type="primary" loading ghost circle></z-button>`.trim(),
       };
     },
     methods: {
